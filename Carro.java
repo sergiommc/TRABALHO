f69e33a98a411b4dc5284947626313d8ab7e9d83
sergiommc <sergiomatosmoreira@hotmail.com>
2014-11-25 22:05:51 -0300

    Create Carro.java

---
+++ Carro.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package veiculos;
+
+/**
+ *
+ * @author 14040201
+ */
+public class Carro extends Moto {
+    protected String modelo;
+    public Carro(int p, int c){
+        super(p,c);
+    }
+    
+}
