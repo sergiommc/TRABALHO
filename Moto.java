3c0c461fbcf63b09b9e2dd8ce13361d949f0e59c
sergiommc <sergiomatosmoreira@hotmail.com>
2014-11-26 21:18:58 -0300

    Update Moto.java

--- Moto.java
+++ Moto.java
@@ -1,32 +1,13 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package veiculos;
 
 /**
  *
  * @author 14040201
  */
-public class Moto {
-    protected int placa;
-    protected int carteiraMotorista;
+public class Moto extends Veiculo {
     
-    public Moto(int p, int c){
-        setPlaca(p);
-        setCarteiraMotorista(c);
-    }
-    public void setPlaca(int p){
-        this.placa=p;
-    }
-    public int getPlaca(){
-        return this.placa;
-    }
-    public void setCarteiraMotorista(int c){
-        this.carteiraMotorista = c;
-    }
-    public int getCarteiraMotorista(){
-        return this.carteiraMotorista;
+	public Moto(int p, int c){
+    	super(p, c);
     }
     
 }
