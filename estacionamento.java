c660f1ae38fc5dce7b3bb205da7988dc0f6f1838
sergiommc <sergiomatosmoreira@hotmail.com>
2014-11-28 22:12:48 -0300

    Update estacionamento.java

--- estacionamento.java
+++ estacionamento.java
@@ -9,10 +9,11 @@ import javax.swing.JOptionPane;
  * @author 14040201
  */
 public class Estacionamento {
-	private ArrayList<Moto> listaMotos = new ArrayList<Moto>();
-	private ArrayList<Carro> listaCarros = new ArrayList<Carro>();
-	private ArrayList<Veiculo> Veiculos = new ArrayList<Veiculo>();
-	private String listaEstacionados;
+	
+	
+        private ArrayList<Veiculo> listaCarros = new ArrayList<Veiculo>();
+        private ArrayList<Veiculo> listaMotos = new ArrayList<Veiculo>();
+	Double diaria = 20.0;
 	//inicio do metodo main
 	public static void main(String Args[]) {
 		
@@ -23,11 +24,10 @@ public class Estacionamento {
 		int c;
 		
 		Carro carro;
-		String modelo;
 
 		int opcao = 0; //menu
-		while (opcao != 8) {
-			opcao = Integer.parseInt(JOptionPane.showInputDialog("Entre com a opção desejada: \n 1-Cadastrar Moto \n 2- Cadastrar Carro \n 3- Remover moto \n 4- Remover carro \n 5- Consultar Moto \n 6- Consultar Carro \n 7- Listar todos os Veiculos \n 8- sair"));
+		while (opcao != 7) {
+			opcao = Integer.parseInt(JOptionPane.showInputDialog("Entre com a opção desejada: \n 1-Cadastrar Moto \n 2- Cadastrar Carro \n 3- Remover moto \n 4- Remover carro \n 5- Consultar Moto \n 6- Consultar Carro \n 7- sair"));
 			if (opcao == 1) { // cadastrar moto
 				p = Integer.parseInt(JOptionPane.showInputDialog("Entre com a placa da moto"));
 				c = Integer.parseInt(JOptionPane.showInputDialog("Entre com o número da CNH"));
@@ -56,9 +56,7 @@ public class Estacionamento {
 				c = Integer.parseInt(JOptionPane.showInputDialog("Entre com o número da CNH"));
 				estacionamento.consultarCarro(c);
 			}
-			if(opcao == 7){//listar veiculos
-				estacionamento.getListaEstacionados();
-			}
+			
 			
 		
 		
@@ -71,6 +69,7 @@ public class Estacionamento {
 	}
 
 	public void removerMoto(int carteiraMotorista) {
+                boolean foiLocalizado = false;
 		// pesquisar no estacionamento
 		// lista vazia
 		if (this.listaMotos.isEmpty()) {
@@ -86,17 +85,16 @@ public class Estacionamento {
 											// salva em plc
 				// se encontrar mostre
 				if (cnh == carteiraMotorista) {
-					JOptionPane.showMessageDialog(
-							null,
-							"Moto localizada: " + moto.getPlaca()
-									+ "Motorista: "
-									+ moto.getCarteiraMotorista());
+					JOptionPane.showMessageDialog(null, "Moto localizada: " + moto.getPlaca()+ " Motorista: "+ moto.getCarteiraMotorista()+" Valor a ser pago é "+moto.getValorEstacionamento(diaria));
 					this.listaMotos.remove(i);
-					JOptionPane.showMessageDialog(null, moto.getPlaca()
-							+ "Entregue com sucesso");
+					JOptionPane.showMessageDialog(null, moto.getPlaca()+ " Entregue com sucesso");
+                                        foiLocalizado = true;
 				}
+                                
 			}// retorna se não encontrar
-			JOptionPane.showMessageDialog(null, "Moto não encontrada");
+                        if(!foiLocalizado)
+                                {JOptionPane.showMessageDialog(null, "Moto não encontrada");}
+			
 		}
 	}
 
@@ -107,9 +105,9 @@ public class Estacionamento {
 	}
 
 	public void removerCarro(int carteiraMotorista) {
+             boolean foiLocalizado = false;
 		if (this.listaCarros.isEmpty()) {
-			JOptionPane.showMessageDialog(null,
-					"Erro na busca: Nenhum carro estacionado.");
+			JOptionPane.showMessageDialog(null,"Erro na busca: Nenhum carro estacionado.");
 		}// senão... procurar moto
 		else {
 			for (int i = 0; i < listaCarros.size(); i++) {
@@ -117,21 +115,21 @@ public class Estacionamento {
 				int cnh = carro.getCarteiraMotorista();
 				// se encontrar mostre
 				if (cnh == carteiraMotorista) {
-					JOptionPane.showMessageDialog(
-							null,
-							"Carro localizado: " + carro.getPlaca()
-									+ "Motorista: "
-									+ carro.getCarteiraMotorista());
+					JOptionPane.showMessageDialog(null, "Carro localizado: " + carro.getPlaca()+ "Motorista: "+ carro.getCarteiraMotorista()+" Valor a ser pago é "+carro.getValorEstacionamento(diaria));
 					this.listaCarros.remove(i);
-					JOptionPane.showMessageDialog(null, carro.getPlaca()
-							+ "Entregue com sucesso");
-				}
+					JOptionPane.showMessageDialog(null, carro.getPlaca()+ " Entregue com sucesso");
+                                        foiLocalizado = true;
+				}       
 			}
+                        if(!foiLocalizado){
+                                JOptionPane.showMessageDialog(null, "Carro não encontrado");
+                                }
 		}
-		JOptionPane.showMessageDialog(null, "Carro não encontrado");
+		
 	}
 
 	public void consultarMoto(int carteiraMotorista) {
+                boolean foiLocalizado = false;
 		if (this.listaMotos.isEmpty()) {
 			JOptionPane.showMessageDialog(null,
 					"Erro na busca: Nenhuma moto estacionada.");
@@ -147,12 +145,17 @@ public class Estacionamento {
 				if (cnh == carteiraMotorista) {
 					JOptionPane.showMessageDialog(null, "Moto localizada: "
 							+ moto.getPlaca());
+                                        foiLocalizado = true;
 				}
-			}
+                        }
+                        if(!foiLocalizado){
+                                JOptionPane.showMessageDialog(null, "Moto não encontrada");
+                                }
 		}
 	}
 
 	public void consultarCarro(int carteiraMotorista) {
+             boolean foiLocalizado = false;
 		if (this.listaCarros.isEmpty()) {
 			JOptionPane.showMessageDialog(null,
 					"Erro na busca: Nenhum carro estacionado.");
@@ -166,22 +169,14 @@ public class Estacionamento {
 											// salva em plc
 				// se encontrar mostre
 				if (cnh == carteiraMotorista) {
-					JOptionPane.showMessageDialog(null, "Carro localizado: "
-							+ carro.getPlaca());
+					JOptionPane.showMessageDialog(null, "Carro localizado: "+ carro.getPlaca());
+                                        foiLocalizado = true;
 				}
-			}
+                        }
+                        if(!foiLocalizado){
+                                JOptionPane.showMessageDialog(null, "Carro não encontrado");
+                                }
 		}
 	}
-	public String getListaEstacionados(){
-		this.Veiculos.addAll(listaMotos);
-		this.Veiculos.addAll(listaCarros);
-		if(Veiculos.isEmpty()){
-			return "Não há nenhum carro ou moto estacionado";
-		}
-		else{
-			for (int i = 0; i < Veiculos.size(); i++) {
-				listaEstacionados = listaEstacionados + Veiculos.get(i).getPlaca() + "\n";
-			}
-		}return listaEstacionados;
-	}
+	
 }
