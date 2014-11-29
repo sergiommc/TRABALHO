99c5a20e6b473aba8c0973d40cd45dd6fa50ebfc
sergiommc <sergiomatosmoreira@hotmail.com>
2014-11-28 22:09:35 -0300

    Update Carro.java

--- Carro.java
+++ Carro.java
@@ -1,10 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package veiculos;
 
-
+/**
+ *
+ * @author 14040201
+ */
 public class Carro extends Veiculo {
-    
     public Carro(int p, int c){
         super(p,c);
+        tarifa = 4.5;
+        
     }
     
 }
